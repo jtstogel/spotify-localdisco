83eb8f564a2bd1a04922a4195bac54e3f8b62795
Your Name <you@example.com>
2023-12-22 17:01:08 -0800

    obfuscate Env.hs with pointless & monads

--- src/Env.hs
+++ src/Env.hs
@@ -9,40 +9,44 @@ import qualified Data.Text as T
 
 import Data.Either ()
 import Data.Maybe (fromMaybe)
-import Data.Text (Text(..))
+import Data.Text (Text)
 
 data Env = Env
   { spotifyClientID :: Text
   , spotifyClientSecret :: Text
   } deriving (Show)
 
-buildEnv :: Map.Map String String ->  Either String Env
-buildEnv env = do
-  let get = \k -> eitherFromMaybe ("Failed to find env key " ++ k) $ Map.lookup k env
-
+buildEnv :: Monad m => (String -> m Text) -> m Env
+buildEnv get = do
   sClientID <- get "SPOTIFY_CLIENT_ID"
   sClientSecret <- get "SPOTIFY_CLIENT_SECRET"
   return $ Env
-    { spotifyClientID = T.pack sClientID
-    , spotifyClientSecret = T.pack sClientSecret
+    { spotifyClientID = sClientID
+    , spotifyClientSecret = sClientSecret
     }
 
 eitherFromMaybe :: l -> Maybe r -> Either l r
-eitherFromMaybe l = fromMaybe (Left l) . (fmap Right)
+eitherFromMaybe = (. fmap Right) . fromMaybe . Left
 
-parseLine :: String -> Maybe (String, String)
-parseLine line = do
-  i <- List.elemIndex '=' line
-  return (take i line, drop (i+1) line)
+second :: (b -> b') -> (a, b) -> (a, b')
+second f (a, b) = (a, f b)
 
-parseEnv :: String -> Either String Env
+parseLine :: String -> Maybe (String, Text)
+parseLine line = second (T.pack . tail) . flip List.splitAt line <$> List.elemIndex '=' line
+
+parseEnv :: String -> Either String (Map.Map String Text)
 parseEnv text = do
-  keyValues <- eitherFromMaybe "Failed to parse env lines" . mapM parseLine . List.lines $ text
-  buildEnv $ Map.fromList keyValues
+  keyValues <- eitherFromMaybe "failed to parse env lines" . mapM parseLine . List.lines $ text
+  return $ Map.fromList keyValues
+
+eitherIO :: Either String a -> IO a
+eitherIO = either fail return
 
 load :: String -> IO Env
 load filename = do
     content <- readFile filename
-    case parseEnv content of
-        Right env -> return env
-        Left msg -> error msg
+
+    envMap <- eitherIO $ parseEnv content
+    let get k = eitherFromMaybe ("failed to find env key " ++ k) $ Map.lookup k envMap
+
+    eitherIO $ buildEnv get
